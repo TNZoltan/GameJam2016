ba4f1a38ad91ebffefabdee8391c3a95ab20c07e
Shapour Nemati <shapournemati@gmail.com>
2016-09-16 21:55:38 +0200

    UI for rps is now linked to a ChoicesHandler object

---
+++ Assets/Scripts/ChoicesHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoicesHandler : MonoBehaviour {
+
+	public string hornChoice, tailChoice;
+
+	public void SetHornChoice(string choice)
+	{
+		hornChoice = choice;
+	}
+
+	public void SetTailChoice(string choice)
+	{
+		tailChoice = choice;
+	}
+}
