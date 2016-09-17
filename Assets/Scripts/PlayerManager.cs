a663cbc3a876186fcef26a6ba99d1e8bb81f2f2c
Shapour Nemati <shapournemati@gmail.com>
2016-09-17 02:21:57 +0200

    Decoupled players collision and level logic

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,15 +6,10 @@ public class PlayerManager : MonoBehaviour {
 
 	public void OnTriggerEnter2D(Collider2D other)
 	{
-
-		int a = GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().CalculateCollision ();
-		if (a!=-1)
-			Debug.Log(a);
 	}
 
 	public void OnTriggerExit2D(Collider2D other)
 	{
-		GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().MatchEnd ();
 	}
 
 	// Use this for initialization
---
+++ Assets/Scripts/SceneDuration.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class SceneDuration: MonoBehaviour {
+
+	ChoicesHandler ch;
+	public float timer = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+		ch = GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ();
+		ch.CalculateCollision ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0)
+			ch.MatchEnd ();
+	}
+}
