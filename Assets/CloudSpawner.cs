78e80c4a17a876688c1ac0a00bb32f9df9ecc099
Shapour Nemati <shapournemati@gmail.com>
2016-09-17 10:57:54 +0200

    Correctly displaying user choices and clouds are spawned randomly

---
+++ Assets/CloudSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoint;
+	public GameObject[] clouds;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject g in spawnPoint) {
+			if (Random.Range (0, 9)<6) {
+				Instantiate(clouds[Random.Range(0,clouds.Length-1)],g.transform);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PlayerChoicesDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerChoicesDisplayer : MonoBehaviour {
+
+	public GameObject p1horn;
+	public GameObject p1tail;
+	public GameObject p2horn;
+	public GameObject p2tail;
+
+	// Use this for initialization
+	void Start () {
+		p1horn.GetComponent<Text>().text += GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().player1Horn;	
+		p1tail.GetComponent<Text>().text += GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().player1Tail;	
+		p2horn.GetComponent<Text>().text += GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().player2Horn;	
+		p2tail.GetComponent<Text>().text += GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ().player2Tail;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
