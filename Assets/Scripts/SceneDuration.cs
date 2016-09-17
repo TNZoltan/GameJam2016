fe4e9dc7568c0502a0959318536af99083e190c9
Shapour Nemati <shapournemati@gmail.com>
2016-09-17 09:35:29 +0200

    Polished some UI

--- Assets/Scripts/SceneDuration.cs
+++ Assets/Scripts/SceneDuration.cs
@@ -1,22 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Threading;
+using UnityEngine.UI;
 
 public class SceneDuration: MonoBehaviour {
 
 	ChoicesHandler ch;
-	public float timer = 4f;
+	public float timer = 6f;
 
 	// Use this for initialization
 	void Start () {
 		ch = GameObject.Find ("ChoiceHandler").GetComponent<ChoicesHandler> ();
-		ch.CalculateCollision ();
+		int result = ch.CalculateCollision ();
+		if (result != 0)
+			GameObject.Find ("EndRoundText").GetComponent<Text> ().text = "player " + result + "\nwon the round";
+		else
+			GameObject.Find ("EndRoundText").GetComponent<Text> ().text = "It's a draw \nfor this round";
 	}
 
 	// Update is called once per frame
 	void Update () {
 		timer -= Time.deltaTime;
-		if (timer <= 0)
+		if (timer <= 0) {
+			timer = 6f;
 			ch.MatchEnd ();
+		}
 	}
 }
