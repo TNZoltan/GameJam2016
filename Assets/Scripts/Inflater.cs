10fa680456e7eab65b843e447ebcfc3b9038d854
Shapour Nemati <shapournemati@gmail.com>
2016-09-17 18:09:24 +0200

    Added and sliced other sprites for the players

--- Assets/Scripts/Inflater.cs
+++ Assets/Scripts/Inflater.cs
@@ -8,13 +8,19 @@ public class Inflater : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!locked) {
 			this.transform.localScale += new Vector3(speed,speed,0);
+			if (name == "EndRoundThing") {
+				Color temp = GetComponent<Image>().color;
+				temp.a+=1;
+				GetComponent<Image>().color = temp;
+
+			}
 		}
 	}
 }
