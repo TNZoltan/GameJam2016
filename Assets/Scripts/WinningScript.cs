53989709337a01914e4c770a37033ce625f99c8d
TNZoltan <TNZoltan@github.com>
2016-09-18 10:57:13 +0200

    Done winning scene. No time for making it pretty :(

--- Assets/Scripts/WinningScript.cs
+++ Assets/Scripts/WinningScript.cs
@@ -6,19 +6,18 @@ public class WinningScript : MonoBehaviour {
     ChoicesHandler ch;
 	// Use this for initialization
 	void Start () {
-        ch = GameObject.Find("Choicehandler").GetComponent<ChoicesHandler>();
-        if (ch.scoreHandler.player1Score > ch.scoreHandler.player2Score) {
-            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite =Resources.Load<Sprite>("p1background.jpg");
-            GameObject.Find("WinText").GetComponent<Text>().text = "";
-        } else {
-            GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("p2background.jpg"); ;
-            GameObject.Find("WinText").GetComponent<Text>().text = "";
-        }
-	    
-	}
+            ch = GameObject.Find("Choicehandler").GetComponent<ChoicesHandler>();
+            if (ch.scoreHandler.player1Score > ch.scoreHandler.player2Score) {
+                GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("p1background.jpg");
+                GameObject.Find("WinText").GetComponent<Text>().text = "Congratulations Player 1, you won! \n You are the new king of Unicornland!";
+            } else {
+                GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("p2background.jpg"); ;
+                GameObject.Find("WinText").GetComponent<Text>().text = "Congratulations Player 2, you won! \n You are the new king of Unicornland!";
+            }
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
